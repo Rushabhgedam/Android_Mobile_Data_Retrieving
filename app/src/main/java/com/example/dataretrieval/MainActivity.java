927b1ebafb55c0791716a0c6ac43aaf1b942cd71
RushabhGedam <rushabhg358@gmail.com>
2019-05-22 20:33:58 +0100

    Test9

--- app/src/main/java/com/example/dataretrieval/MainActivity.java
+++ app/src/main/java/com/example/dataretrieval/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
+            //                                        int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             return;
